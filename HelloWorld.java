fba616f42deda7a02b2a4fb70fb4b674112731d9
Marcos José Villafañe <marcosjvillafane@gmail.com>
2024-07-31 04:43:12 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Primer archivo en Java modificacion desde la pagina git");
+        System.out.println("Primer archivo en Java modificacion desde VSCode");
     }
 }
